3109ad5a25e817ac5e5895433d067d7057bb05f0
AnKIbach <automatai@proton.me>
2026-04-15 20:57:06 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // relative paths for github pages
+  base: '/White-trash-trip/', 
 })
